0401ac8c976566a8b22f156f01c0147a944fe795
Rishi Pratap <rp0258@srmist.edu.in>
2023-05-20 11:51:56 +0530

    fix bug

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,9 +1,9 @@
 import Prompt from "@models/prompt";
-import { connectToDB } from "@utils/database";
+import { connectToDatabase } from "@utils/database";
 
 export const GET = async (request) => {
     try {
-        await connectToDB()
+        await connectToDatabase()
 
         const prompts = await Prompt.find({}).populate('creator')
 
