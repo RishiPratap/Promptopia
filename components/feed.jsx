eb7f771bd697db042d2a3dd656db7d44f4dcc009
Rishi Pratap <rp0258@srmist.edu.in>
2023-05-20 10:04:26 +0530

    view post

--- components/feed.jsx
+++ components/feed.jsx
@@ -1,7 +1,58 @@
-import React from 'react'
+"use client";
+
+import PromptCard from '@components/promptcard'
+import {useState,useEffect} from 'react'
+
+function PromptCardList({data,handleTagClick}) {
+  return (
+    <div className='mt-16 prompt_layout'>
+     {
+      data.map((prompt) => (
+        <PromptCard
+          key={prompt._id}
+          prompt={prompt}
+          handleTagClick={handleTagClick}
+        />
+      ))
+     }
+    </div>
+  )
+}
 
 export default function Feed() {
+  const [searchtext, setSearchText] = useState('');
+  const [posts, setPosts] = useState([]);
+
+  const handleSearchTextChange = (e) => {
+    setSearchText(e.target.value)
+  }
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch(`/api/prompt`)
+      const data = await res.json()
+      setPosts(data);
+      console.log(data)
+    }
+    fetchPosts()
+  }, [])
+
   return (
-    <div>Feed</div>
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input
+          type='text'
+          placeholder='What are you thinking?'
+          value={searchtext}
+          onChange={handleSearchTextChange}
+          required
+          className='search_input peer'
+        />
+      </form>
+      <PromptCardList
+      data = {posts}
+      handleTagClick = {() => {}}
+      />
+    </section>
   )
 }
